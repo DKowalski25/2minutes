329ad151311a8806caf3d2c4cfb9be200c0d41df
DKowalski25 <dshinoda25@gmail.com>
2025-02-15 02:07:06 +0300

    Added AuthController

---
+++ notetaking/src/main/java/dev/twominutes/notetaking/controller/AuthController.java
@@ -0,0 +1,18 @@
+package dev.twominutes.notetaking.controller;
+
+import dev.twominutes.notetaking.models.User;
+import dev.twominutes.notetaking.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    private UserService userService;
+
+    public User registerUser(@RequestBody User user) {
+        return userService.RegisterUser(user);
+    }
+}
