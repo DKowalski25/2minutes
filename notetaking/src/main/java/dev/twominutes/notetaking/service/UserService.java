b4e2efbb1482d1d43783f16819f151e5d250556e
DKowalski25 <dshinoda25@gmail.com>
2025-02-15 02:06:54 +0300

    Added UserService

---
+++ notetaking/src/main/java/dev/twominutes/notetaking/service/UserService.java
@@ -0,0 +1,33 @@
+package dev.twominutes.notetaking.service;
+
+import dev.twominutes.notetaking.models.User;
+import dev.twominutes.notetaking.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User RegisterUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+}
