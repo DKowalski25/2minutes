a43e49c675c22ee53e07d7e38fc75518114773cd
DKowalski25 <dshinoda25@gmail.com>
2025-02-18 01:37:03 +0300

    Added access modifier "final" to var JwtUtil and UserDetailsService; Added permit to "/authenticate/**"

--- notetaking/src/main/java/dev/twominutes/notetaking/config/SecurityConfig.java
+++ notetaking/src/main/java/dev/twominutes/notetaking/config/SecurityConfig.java
@@ -9,25 +9,27 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import dev.twominutes.notetaking.service.CustomUserDetailService;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final UserDetailsService userDetailsService;
+    private final CustomUserDetailService userDetailsService;
     private final JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable()) // Отключаем CSRF
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll() // Разрешаем доступ без аутентификации
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/authenticate/**").permitAll()// Разрешаем доступ без аутентификации
                         .anyRequest().authenticated() // Все остальные запросы требуют аутентификации
                 )
                 .sessionManagement(session -> session
