b808cdbfb473020d1417a60b2235fc9ea4d03e77
DKowalski25 <dshinoda25@gmail.com>
2025-02-17 03:39:39 +0300

    Created AuthenticationController

---
+++ notetaking/src/main/java/dev/twominutes/notetaking/controller/AuthenticationController.java
@@ -0,0 +1,45 @@
+package dev.twominutes.notetaking.controller;
+
+import dev.twominutes.notetaking.models.AuthenticationRequest;
+import dev.twominutes.notetaking.models.AuthenticationResponse;
+import dev.twominutes.notetaking.utils.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+            throws Exception {
+        if (authenticationRequest.getUsername() == null || authenticationRequest.getPassword() == null) {
+            throw new IllegalArgumentException("Username or password cannot be null");
+        }
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            authenticationRequest.getUsername(),
+                            authenticationRequest.getPassword()
+                    )
+            );
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("Invalid username/password supplied");
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+
+}
