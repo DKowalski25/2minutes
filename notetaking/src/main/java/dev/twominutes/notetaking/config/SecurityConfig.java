17c643077f384229e914690153028df8964802fc
DKowalski25 <dshinoda25@gmail.com>
2025-02-24 02:39:43 +0300

    R

--- notetaking/src/main/java/dev/twominutes/notetaking/config/SecurityConfig.java
+++ notetaking/src/main/java/dev/twominutes/notetaking/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package dev.twominutes.notetaking.config;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
