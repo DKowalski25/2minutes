d014078746f7bd56eebd3fe59c76a3acffeed9cf
DKowalski25 <dshinoda25@gmail.com>
2025-02-25 02:31:06 +0300

    Changed the data model that the interface works with

--- notetaking/src/main/java/dev/twominutes/notetaking/service/note/NoteService.java
+++ notetaking/src/main/java/dev/twominutes/notetaking/service/note/NoteService.java
@@ -1,14 +1,14 @@
 package dev.twominutes.notetaking.service.note;
 
-import dev.twominutes.notetaking.models.Note;
+import dev.twominutes.notetaking.dto.note.NoteDto;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface NoteService {
-    Note save (Note note);
-    Optional<Note> findById(Long id);
-    List<Note> findByUserId(Long userId);
-    void update(Note note);
+    NoteDto save(NoteDto noteDto);
+    Optional<NoteDto> findById(Long id);
+    List<NoteDto> findByUserId(Long userId);
+    void update(NoteDto noteDto);
     void deleteById(Long id);
 }
