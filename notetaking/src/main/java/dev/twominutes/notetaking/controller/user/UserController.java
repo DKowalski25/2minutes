33205d5247cc14fe4dff930456c9e4c5e752726c
DKowalski25 <dshinoda25@gmail.com>
2025-02-24 03:55:06 +0300

    Added Swagger annotation to the user controller

--- notetaking/src/main/java/dev/twominutes/notetaking/controller/user/UserController.java
+++ notetaking/src/main/java/dev/twominutes/notetaking/controller/user/UserController.java
@@ -3,10 +3,22 @@ package dev.twominutes.notetaking.controller.user;
 import dev.twominutes.notetaking.dto.user.UserRequestDto;
 import dev.twominutes.notetaking.dto.user.UserResponseDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "User Registration", description = "Endpoints for user registration")
 @RequestMapping("/api/auth")
 public interface UserController {
+    @Operation(
+            summary = "Register a new user",
+            description = "Registers a new user with the provided user details",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "User registered successfully"),
+                    @ApiResponse(responseCode = "400", description = "Bad request")
+            }
+    )
     @PostMapping("/register")
     UserResponseDto registerUser(@RequestBody UserRequestDto userDto);
 }
