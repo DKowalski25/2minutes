9d17bb7b6980f5054fa897cb2074bcee2ea7d03b
DKowalski25 <dshinoda25@gmail.com>
2025-02-25 02:31:52 +0300

    changed the data model from note model to NoteDto model, added NoteMapper

--- notetaking/src/main/java/dev/twominutes/notetaking/service/note/NoteServiceImpl.java
+++ notetaking/src/main/java/dev/twominutes/notetaking/service/note/NoteServiceImpl.java
@@ -1,36 +1,49 @@
 package dev.twominutes.notetaking.service.note;
 
+import dev.twominutes.notetaking.dto.note.NoteDto;
+import dev.twominutes.notetaking.dto.note.NoteMapper;
 import dev.twominutes.notetaking.models.Note;
 import dev.twominutes.notetaking.repository.note.NoteRepository;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class NoteServiceImpl implements NoteService{
 
     private final NoteRepository noteRepository;
+    private final NoteMapper noteMapper;
 
     @Override
-    public Note save(Note note) {
-        return noteRepository.save(note);
+    public NoteDto save(NoteDto noteDto) {
+        Note note = noteMapper.toEntity(noteDto);
+        note = noteRepository.save(note);
+        return noteMapper.toDto(note);
     }
 
     @Override
-    public Optional<Note> findById(Long id) {
-        return noteRepository.findById(id);
+    public Optional<NoteDto> findById(Long id) {
+        Optional<Note> note = noteRepository.findById(id);
+        return note.map(noteMapper::toDto);
     }
 
     @Override
-    public List<Note> findByUserId(Long userId) {
-        return noteRepository.findByUserId(userId);
+    public List<NoteDto> findByUserId(Long userId) {
+        List<Note> notes = noteRepository.findByUserId(userId);
+        return notes.stream()
+                .map(noteMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void update(Note note) {
+    public void update(NoteDto noteDto) {
+        Note note = noteMapper.toEntity(noteDto);
         noteRepository.update(note);
     }
 
